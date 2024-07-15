ccb7804134ab77eff0cd22f7a014f4e2d9152815
Yousef Mohamed <yousefmohamed@Yousefs-MacBook-Air.local>
2024-07-15 12:20:43 +0300

    Updated layout for avatar and author name alignment and added avatar image

--- src/app/page.js
+++ src/app/page.js
@@ -3,20 +3,30 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="bg-[#F4D04E] min-h-screen flex items-center justify-center">
-      <div className="bg-white w-[384px] h-[522px] rounded-[20px] outline outline-black outline-1 shadow flex flex-col items-center justify-center relative" style={{boxShadow: '10px 10px'}}>
-        <div className="absolute top-5">
+      <div className="bg-white w-[384px] h-[522px] rounded-[20px] outline outline-black outline-1 shadow flex flex-col items-center" style={{boxShadow: '10px 10px'}}>
+        <div className="mt-5">
           <img src="image.png" alt="blog image" className="w-[336px] h-[200px] rounded-[10px]" />
         </div>
 
-        
-          <div className="bg-[#F4D04E] mr-[247px] w-[88px] h-[29px] flex items-center justify-center rounded-[4px]">
-            <p className="font-extrabold tracking-tight">Learning</p>
-          </div>
+        <div className="w-full gap-3 flex flex-col ml-12 mt-5">
+            <div className="bg-[#F4D04E] mr-[247px] w-[88px] h-[29px] flex items-center justify-center rounded-[4px]">
+              <p className="font-semibold tracking-tight text-[15px] ">Learning</p>
+            </div>
 
-          <div className="mr-[180px] ">
-            <p className="text-sm">Published 21 Dec 2023</p>
+            <div>
+              <p className="text-sm">Published 21 Dec 2023</p>
+            </div>
+            <div>
+              <h1 className="font-bold text-[23px] active:text-[#F4D04E]">HTML & CSS foundations</h1>
+            </div>
+            <div>
+              <p className="text-[#7F7F7F]">These languages are the backbone of every website, defining structure, content, and presentation.</p>
+            </div>
+            <div className="flex items-center">
+              <img src="avatar.webp" className="w-[32px] h-[32px] mt-6"></img>
+              <h1 className="font-bold text-[13px] mt-6 ml-3">Greg Hooper</h1>
+            </div>
           </div>
-        
       </div>
     </div>
   );
